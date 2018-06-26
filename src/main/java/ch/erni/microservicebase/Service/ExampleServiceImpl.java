e05752c96a07ba956f61778c34352500aaa01de1
Michel Meyer <meyer_michel@msn.com>
2018-06-26 16:46:39 +0200

    Finish all todos

--- src/main/java/ch/erni/microservicebase/Service/ExampleServiceImpl.java
+++ src/main/java/ch/erni/microservicebase/Service/ExampleServiceImpl.java
@@ -1,24 +1,20 @@
 package ch.erni.microservicebase.Service;
 
-import ch.erni.microservicebase.Controller.ExampleController;
 import ch.erni.microservicebase.Model.Example;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExampleServiceImpl implements ExampleService {
 
-    //TODO 02 remove all formatting errors manually or if you are able to do so remove them with the eclipse formatter
-    //TODO 04 remove all Bugs and technical debts findbugs tell you to do.
 
     public Example getCompletedExample(String uncompleteExample) {
         Example example = new Example();
-        if(uncompleteExample != null){
-            String completedExample = uncompleteExample.replaceAll("an", "");
+        if (uncompleteExample != null) {
+            String completedExample = uncompleteExample.replaceAll("un", "");
             example = new Example();
             example.setCompletedExample(completedExample);
             return example;
-        }else
-        {
+        } else {
             throw new NullPointerException();
         }
     }
